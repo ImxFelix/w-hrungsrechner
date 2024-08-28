d8e5913929a6606a2a7fe657d8820b8dea6e1768
ImxFelix <2836@protonmail.com>
2024-08-28 13:54:43 +0200

    Zweite Session

--- main.py
+++ main.py
@@ -9,24 +9,53 @@ def get_exchange_rate(cur_name):
     return exchange_rate
 
 
+def dropdown_from_currency_changed(var, index, mode):
+    print("dropdown_from_currency_changed")
+
+def dropdown_to_currency_changed(var, index, mode):
+    print("dropdown_to_currency_changed")
+
+
+
+
+def callback(self, P):
+    if str.isdigit(P) or P == "" or P == ".":
+        return True
+    else:
+        return False
 
 def button_click():
     l2.config(text=get_exchange_rate(e1.get()))
 
 mainWindow = tk.Tk()
 mainWindow.title('Währungsrechner')
+mainWindow.geometry('300x300')
 
-l1 = tk.Label(mainWindow, text="Währung: ")
-l1.grid(row=0, column=0)
-
-l2 = tk.Label(mainWindow, text="0")
-l2.grid(row=0, column=1)
+vcmd = (mainWindow.register(callback))
 
 
-e1 = tk.Entry(mainWindow)
-e1.grid(row=1, column=1)
+# Erstes Dropdown
+dropdown_from_currency_clicked = tk.StringVar()
+dropdown_from_currency_clicked.set(options[8])
+dropdown_from_currency_clicked.trace_add("write", dropdown_from_currency_changed)
+dropdown_from_currency = tk.OptionMenu(mainWindow, dropdown_from_currency_clicked, *options)
+dropdown_from_currency.grid(row=0, column=0)
 
-button = tk.Button(mainWindow, text="Umrechnen", command=button_click)
-button.grid()
+# Zweites Dropdown
+dropdown_to_currency_clicked = tk.StringVar()
+dropdown_to_currency_clicked.set(options[31])
+dropdown_to_currency_clicked.trace_add("write", dropdown_to_currency_changed)
+dropdown_to_currency = tk.OptionMenu(mainWindow, dropdown_to_currency_clicked, *options)
+dropdown_to_currency.grid(row=0, column=2)
+
+# Erstes Eingabefeld
+input_from_currency = tk.Entry(mainWindow)
+input_from_currency.grid(row=1, column=0)
+
+
+# Zweites Eingabefeld
+input_to_currency = tk.Entry(mainWindow , validate='all', validatecommand=(vcmd, '%P'))
+
+input_to_currency.grid(row=1, column=2)
 
 mainWindow.mainloop()
